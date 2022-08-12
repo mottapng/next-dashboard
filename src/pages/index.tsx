232afedb659c822d284025d1d1afe153bc31cd55
Victor Motta <victormotta7@gmail.com>
2022-08-12 18:48:09 -0300

    header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import { Input } from '../components/Form/Input'
 export default function SignIn() {
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
-      <Flex as="form" w="100%" maxWidth={360} bg="gray.800" p={8} borderRadius={8} flexDir="column">
-        <Stack spacing={4}>
+      <Flex as="form" w="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDir="column">
+        <Stack spacing="4">
 
           <Input name="email" label="E-mail" type="email" />
           <Input name="password" label="Password" type="password" />
